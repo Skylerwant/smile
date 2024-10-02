ebc0f91da3d2a0a070f7381d38ed69784170ca83
Skylerwant <123miin@gmail.com>
2024-10-02 11:31:40 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  ellipse(200, 200, 200, 200); 
+  arc(200,200,100,100,0,PI);
+  //arc(200,270,100,100,PI,2*PI)
+  line(195,200,205,200);
+  ellipse(170,170,10,10);
+  ellipse(230,170,10,10);
+ 
+}
